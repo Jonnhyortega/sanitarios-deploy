5a024f95ee6e5c6e02e6006c22fe3f9bd06756f1
Jonathan Ortega <132107501+Jonnhyortega@users.noreply.github.com>
2025-10-07 10:57:13 -0300

    Deploy automatico (SANITARIOS LUGANO) - 2025-10-07 10:57:13

--- assets/index-yBK-B1Ui.js
+++ assets/index-yBK-B1Ui.js
@@ -375,7 +375,7 @@ Error generating stack: `+o.message+`
   align-items: center;
   text-align: center;
   padding: 3rem;
-  background-color: #f9f9f9;
+  background: linear-gradient(var(--blue2) 50%, var(--blue3));
   border-radius: 10px;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
   gap: 2rem;
